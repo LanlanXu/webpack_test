6af7540d27bae547ded7eb87dbdc375a5d95f062
llxu7 <llxu7@iflytek.com>
2021-08-31 16:56:01 +0800

    修改

--- vue-transform-webpack-4/webpack.config.js
+++ vue-transform-webpack-4/webpack.config.js
@@ -1,24 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
 module.exports = {
   mode: 'production',
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, './dist/index.js')
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
-    // rules: [
-    //     {
-    //         test: /\.vue$/,
-    //         loader: 'vue-loader'
-    //     },
-    //     {
-    //         test: /\.js$/,
-    //         loader: 'babel-loader'
-    //     },
-    //     {
-    //         test: /\.css$/,
-    //         loader: 'css-loader'
-    //     }
-    // ]
-  }
+    rules: [
+        {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        },
+        {
+            test: /\.js$/,
+            loader: 'babel-loader'
+        },
+        {
+            test: /\.css$/,
+            loader: 'css-loader'
+        }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html'
+    })
+  ]
 };
+
+
+
+// module.exports = {
+//   mode: 'production', // 会相应的使用内置优化
+//   entry: './index.js', // 入口文件
+//   output: {
+//     filename: '[name].js', // 编译后的文件名
+//     path: path.resolve(__dirname, 'dist'), // 编译后的文件存放的目录，必须是绝对路径
+//     publicPath: '/assets/', // 编译后的文件页面中引用的时候，会拼上啥路径，比如说
+//   }
+
+// }
