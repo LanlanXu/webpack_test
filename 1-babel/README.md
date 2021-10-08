a8214da549b4d69f6ede75360f74d6932416368c
llxu7 <llxu7@iflytek.com>
2021-10-08 15:39:49 +0800

    feat：regenerator-runtime的使用

--- 1-babel/README.md
+++ 1-babel/README.md
@@ -2,6 +2,7 @@
 纯nodejs实现，仅仅依赖：
 @babel/core
 @babel/preset-env
+regenerator-runtime // 如果有async语法使用，或者generator函数使用的话，需要安装
 
 基于nodejs的原生方法使用@babel/core，对文件进行编译。
 其中静态方法和实例方法的垫片会以require（引入corejs）的形式实现。其他基本语法直接转化。
--- 2-webpack/after/target.js
+++ 2-webpack/after/target.js
@@ -1,5 +1,7 @@
 "use strict";
 
+require("regenerator-runtime/runtime.js");
+
 require("core-js/modules/es.array.includes.js");
 
 require("core-js/modules/es.object.to-string.js");
@@ -9,6 +11,12 @@ require("core-js/modules/es.promise.js");
 require("core-js/modules/es.object.assign.js");
 
 require("core-js/modules/es.function.name.js");
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
 var a = 1;
 
@@ -31,3 +39,31 @@ var _name$age = {
     name = _name$age.name,
     age = _name$age.age;
 var name1 = 1;
+
+function helloAsync() {
+  return _helloAsync.apply(this, arguments);
+}
+
+function _helloAsync() {
+  _helloAsync = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            return _context.abrupt("return", "helloAsync");
+
+          case 1:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+  return _helloAsync.apply(this, arguments);
+}
+
+console.log(helloAsync());
+
+var A = function A() {
+  _classCallCheck(this, A);
+};
