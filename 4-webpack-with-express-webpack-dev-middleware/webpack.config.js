971fafe1a23c5bee5cbb2c3c701c42f7065f5f8b
lanlanxu <627363383@qq.com>
2022-04-30 08:33:58 +0800

    feat: 本地husky忘记install导致husky commit lint 未生效

--- 4-webpack-with-express-webpack-dev-middleware/webpack.config.js
+++ 4-webpack-with-express-webpack-dev-middleware/webpack.config.js
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2022-04-29 22:17:22
- * @LastEditTime: 2022-04-29 22:34:20
- * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \webpack_ts\4-webpack-with-express-webpack-dev-middleware\webpack.config.js
- */
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'development',
