f3c60cac26ef78568efa7702fec5c178d2847593
LanlanXu <627363383@qq.com>
2022-05-11 16:23:36 +0800

    feat: 测试使用dotenv及.env文件管理自定义环境变量

--- packages/vue-with-webpack-dev-server-webpack4/index.js
+++ packages/vue-with-webpack-dev-server-webpack4/index.js
@@ -1,16 +1,19 @@
 /*
  * @Author: your name
  * @Date: 2022-05-01 21:32:46
- * @LastEditTime: 2022-05-01 21:39:53
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-05-11 16:18:22
+ * @LastEditors: llxu7
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \webpack_test\packages\vue-with-webpack-dev-server-webpack4\index.js
  */
+require('dotenv').config()
 const webpack = require('webpack')
 const webpackConfig = require('./webpack.config.js')
 const webpackDevServer = require('webpack-dev-server')
 
 const compiler = webpack(webpackConfig)
+console.log(process.env.target)
+
 
 const devServerOptions = {
     ...webpackConfig.devServer,
